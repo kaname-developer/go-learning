3ba35c0ccbb3c099530c353ad50378aa9638b2b7
kaname-developer <c.ob47gl.kk@gmail.com>
2024-11-19 23:13:22 +0900

    learned goroutine

---
+++ concurrency/goroutine.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func showRandNum(n int) {
+	fmt.Println("please wait for a while...")
+
+	// sleep random time
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	time.Sleep(time.Duration(r.Intn(3000)))
+
+	// get random number in [0, n)
+	num := rand.Intn(n)
+	fmt.Printf("random number is %d\n", num)
+}
+
+func main() {
+	// go showRandNum(10) // main goroutin will finish without waiting
+
+	var wg sync.WaitGroup
+	wg.Add(1) // increment counter by 1
+	go func() {
+		defer wg.Done() // decrement counter by one when func() return
+		showRandNum(10)
+	}()
+	wg.Wait() // wait for wg until the counter is 0
+}
